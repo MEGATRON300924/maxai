30df4276dd5044bdc3c0101c8c355548f074341f
Zion Opaaje <zopaaje8@gmail.com>
2026-09-04 20:30:57 +0100

    fix: wire max voice provider dependencies

--- lib/providers/max_voice_provider.dart
+++ lib/providers/max_voice_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../services/max_memory_analyzer.dart';
+import '../services/max_memory_service.dart';
+import '../services/max_voice_memory_service.dart';
 import '../services/max_voice_service.dart';
 
-
 final maxVoiceProvider = Provider<MaxVoiceService>((ref) {
-
-  return MaxVoiceService();
-
+  final memory = MaxMemoryService.instance;
+  final voiceMemory = MaxVoiceMemoryService(memoryService: memory, analyzer: MaxMemoryAnalyzer());
+  return MaxVoiceService(memoryService: voiceMemory);
 });
