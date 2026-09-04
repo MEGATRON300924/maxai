78d1867142397db29ba9551e04918c2960134fad
Zion Opaaje <zopaaje8@gmail.com>
2026-09-04 20:34:23 +0100

    fix: correct app entry auth state handling

--- lib/screens/app_entry.dart
+++ lib/screens/app_entry.dart
@@ -1,98 +1,40 @@
 import 'package:flutter/material.dart';
 
 import '../services/auth_service.dart';
-
-import 'main_navigation_screen.dart';
-import 'onboarding_screen.dart';
-
-
+import 'login_screen.dart';
+import 'home_screen.dart';
 
 class AppEntry extends StatefulWidget {
-
-  const AppEntry({
-    super.key,
-  });
-
+  const AppEntry({super.key});
 
   @override
-  State<AppEntry> createState() =>
-      _AppEntryState();
-
+  State<AppEntry> createState() => _AppEntryState();
 }
 
-
-
-class _AppEntryState
-    extends State<AppEntry> {
-
-
+class _AppEntryState extends State<AppEntry> {
   bool loading = true;
-
   bool loggedIn = false;
-
-
 
   @override
   void initState() {
-
     super.initState();
-
     checkAuth();
-
   }
-
-
 
   Future<void> checkAuth() async {
-
-
-    loggedIn =
-        await AuthService.instance.isLoggedIn;
-
-
-    setState(() {
-
-      loading =
-          false;
-
-    });
-
+    loggedIn = AuthService.instance.isLoggedIn;
+    if (mounted) {
+      setState(() {
+        loading = false;
+      });
+    }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
-
     if (loading) {
-
-      return const Scaffold(
-
-        body:
-            Center(
-
-          child:
-              CircularProgressIndicator(),
-
-        ),
-
-      );
-
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
-
-
-
-    if (loggedIn) {
-
-      return const MainNavigationScreen();
-
-    }
-
-
-
-    return const OnboardingScreen();
-
+    return loggedIn ? const HomeScreen() : const LoginScreen();
   }
-
 }
