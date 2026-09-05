fa2879e9a589b08dfa7f7acabd5925406b3bcc33
Zion Opaaje <zopaaje8@gmail.com>
2026-09-05 14:26:11 +0100

    Simplify MAX wake bridge around wake controller

--- lib/services/max_wake_bridge_service.dart
+++ lib/services/max_wake_bridge_service.dart
@@ -1,24 +1,13 @@
-import 'package:flutter/services.dart';
 import 'max_wake_controller.dart';
 
 /// Compatibility facade for the native MAX wake-word bridge.
 /// New code should prefer [MaxWakeController].
 class MaxWakeBridgeService {
-  static const MethodChannel _channel = MethodChannel('com.maxai/wake');
-  static const EventChannel _events = EventChannel('com.maxai/wake_events');
+  final MaxWakeController _controller = MaxWakeController();
 
-  Stream<Map<String, dynamic>> get wakeEvents =>
-      _events.receiveBroadcastStream().map((event) {
-        if (event is Map) return Map<String, dynamic>.from(event);
-        return <String, dynamic>{'event': event};
-      });
+  Stream<Map<String, dynamic>> get wakeEvents => _controller.wakeEvents;
 
-  Future<bool> startWakeEngine() async {
-    final controller = MaxWakeController();
-    return controller.startWakeEngine();
-  }
+  Future<bool> startWakeEngine() => _controller.startWakeEngine();
 
-  Future<void> stopWakeEngine() async {
-    await _channel.invokeMethod('stopWake');
-  }
+  Future<void> stopWakeEngine() => _controller.stopWakeEngine();
 }
