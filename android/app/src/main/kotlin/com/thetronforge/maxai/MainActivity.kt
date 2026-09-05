1cec62c1e4b6f9ea8b8aef54883bc09f1699b9c8
Zion Opaaje <zopaaje8@gmail.com>
2026-09-05 01:20:03 +0100

    feat: connect MAX wake events to Flutter

--- android/app/src/main/kotlin/com/thetronforge/maxai/MainActivity.kt
+++ android/app/src/main/kotlin/com/thetronforge/maxai/MainActivity.kt
@@ -1,273 +1,84 @@
 package com.thetronforge.maxai
 
-
 import android.os.Bundle
-
 import io.flutter.embedding.android.FlutterActivity
-
 import io.flutter.embedding.engine.FlutterEngine
-
 import io.flutter.plugin.common.EventChannel
-
 import io.flutter.plugin.common.MethodChannel
-
-
-import com.thetronforge.maxai.wake.session.WakeSessionManagerV2
-
-import com.thetronforge.maxai.wake.config.WakeConfiguration
-
-import com.thetronforge.maxai.wake.events.EventDispatcher
-
 import com.thetronforge.maxai.wake.audio.AudioFocusManager
-
+import com.thetronforge.maxai.wake.config.WakeConfiguration
+import com.thetronforge.maxai.wake.events.EventDispatcher
 import com.thetronforge.maxai.wake.optimization.BatteryOptimizer
-
+import com.thetronforge.maxai.wake.session.WakeSessionManagerV2
 import com.thetronforge.maxai.wake.util.Logger
+import com.thetronforge.maxai.wake.util.WakeEngineConstants
 
+class MainActivity : FlutterActivity() {
+    companion object {
+        private const val METHOD_CHANNEL = "com.maxai/wake"
+        private const val EVENT_CHANNEL = "com.maxai/wake_events"
+    }
 
+    private lateinit var wakeManager: WakeSessionManagerV2
+    private val eventDispatcher = EventDispatcher()
+    private var eventSink: EventChannel.EventSink? = null
 
-
-class MainActivity: FlutterActivity(){
-
-
-
-    private val METHOD_CHANNEL =
-
-        "com.maxai/wake"
-
-
-
-
-    private val EVENT_CHANNEL =
-
-        "com.maxai/wake_events"
-
-
-
-
-    private lateinit var wakeManager:
-
-            WakeSessionManagerV2
-
-
-
-
-
-
-    override fun onCreate(
-
-        savedInstanceState:Bundle?
-
-    ){
-
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         initializeWakeEngine()
-
-
     }
 
-
-
-
-
-
-    private fun initializeWakeEngine(){
-
-
-
-        val logger =
-
-            Logger("MAX")
-
-
-
+    private fun initializeWakeEngine() {
         wakeManager = WakeSessionManagerV2(
-
-
             context = this,
-
-
             configuration = WakeConfiguration(),
-
-
-            eventDispatcher = EventDispatcher(),
-
-
-            audioFocusManager =
-
-                AudioFocusManager(this),
-
-
-            batteryOptimizer =
-
-                BatteryOptimizer(this),
-
-
-            logger = logger
-
-
+            eventDispatcher = eventDispatcher,
+            audioFocusManager = AudioFocusManager(this),
+            batteryOptimizer = BatteryOptimizer(this),
+            logger = Logger("MAX")
         )
-
-
     }
 
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-
-
-
-
-
-    override fun configureFlutterEngine(
-
-        flutterEngine:FlutterEngine
-
-    ){
-
-
-        super.configureFlutterEngine(
-
-            flutterEngine
-
-        )
-
-
-
-
-
-        MethodChannel(
-
-            flutterEngine.dartExecutor.binaryMessenger,
-
-            METHOD_CHANNEL
-
-        ).setMethodCallHandler{
-
-
-                call,result ->
-
-
-
-            when(call.method){
-
-
-
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, METHOD_CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
                 "startWake" -> {
-
-
-                    wakeManager.startListening(
-
-                        com.thetronforge.maxai.wake.util.WakeEngineConstants.ListeningMode.BACKGROUND
-
-                    )
-
-
-                    result.success(true)
-
+                    wakeManager.startListening(WakeEngineConstants.ListeningMode.BACKGROUND)
+                    result.success(wakeManager.isActive())
                 }
-
-
-
                 "stopWake" -> {
-
-
                     wakeManager.stopListening()
-
-
                     result.success(true)
-
-
                 }
-
-
-
                 else -> result.notImplemented()
-
-
             }
-
-
-
         }
 
-
-
-
-
-
-
-        EventChannel(
-
-            flutterEngine.dartExecutor.binaryMessenger,
-
-            EVENT_CHANNEL
-
-
-        ).setStreamHandler(
-
-            object:
-
-                EventChannel.StreamHandler{
-
-
-                override fun onListen(
-
-                    args:Any?,
-
-                    events:EventChannel.EventSink?
-
-                ){
-
-                    // wake events stream later
-
-
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, EVENT_CHANNEL).setStreamHandler(
+            object : EventChannel.StreamHandler {
+                private val listener = object : EventDispatcher.Listener {
+                    override fun onEvent(event: Map<String, Any?>) {
+                        runOnUiThread { eventSink?.success(event) }
+                    }
                 }
 
-
-
-                override fun onCancel(
-
-                    args:Any?
-
-                ){
-
+                override fun onListen(args: Any?, events: EventChannel.EventSink?) {
+                    eventSink = events
+                    eventDispatcher.addListener(listener)
                 }
 
-
+                override fun onCancel(args: Any?) {
+                    eventDispatcher.removeListener(listener)
+                    eventSink = null
+                }
             }
-
         )
-
-
-
     }
 
-
-
-
-
-
-
-    override fun onDestroy(){
-
-
-        if(::wakeManager.isInitialized){
-
-
-            wakeManager.cleanup()
-
-
-        }
-
-
-
+    override fun onDestroy() {
+        if (::wakeManager.isInitialized) wakeManager.cleanup()
         super.onDestroy()
-
-
     }
-
-
-
 }
